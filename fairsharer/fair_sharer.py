d135a92545f58fa5d8f98e9580e9f20314e07c24
Emen Fouda <emen.fouda@study.hs-duesseldorf.de>
2026-02-04 20:42:23 +0100

    Fixed ruff formating
    Refactor docstring for clarity and formatting.

--- fairsharer/fair_sharer.py
+++ fairsharer/fair_sharer.py
@@ -1,8 +1,8 @@
 def fair_sharer(values, num_iterations, share=0.1):
     """
     Runs num_iterations.
-    In each iteration the highest value in 'values' gives a fraction (share) to both the left and right neighbor.
-    The leftmost field is considered the neighbor of the rightmost field.
+    In each iteration the highest value in 'values' gives a fraction (share)
+    to both the left and right neighbor.
 
     Examples:
         fair_sharer([0, 1000, 800, 0], 1)  --> [100, 800, 900, 0]
